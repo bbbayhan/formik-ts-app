6a25ec4d20e9672b5da9601a0ae2fb97b11c256b
bbbayhan <bestebbayhan@gmail.com>
2021-03-08 09:25:23 +0100

    Add data structure for the forms and modal to print filled form values by the user

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const data = [
+    {
+        id:"firstName",
+        text:"First Name",
+        type:"text",
+    },
+    {
+        id:"lastName",
+        text:"Last Name",
+        type:"text"
+    },
+    {
+        id:"email",
+        text:"Email",
+        type:"email"
+    }
+];
+
+export const secondData = [
+    {
+        id:"age",
+        name: "age",
+        text:"Age",
+        type:"text",
+    },
+    {
+        id:"birthday",
+        name: "age",
+        text:"Birthday",
+        type:"text"
+    },
+];
+
+export const thirdData = [
+    {
+        id:"companyName",
+        text:"Company Name",
+        type:"text",
+    },
+    {
+        id:"companyYear",
+        text:"Year",
+        type:"text"
+    },
+];
---
+++ src/forms/ForthPage.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import { Dialog, Button, DialogContent, DialogContentText } from '@material-ui/core';
+import { useFormikContext } from 'formik';
+
+export default function ForthPage() {
+  const { values: formValues } = useFormikContext<any>();
+  const [open, setOpen] = useState(false);
+
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" type="submit" onClick={() => setOpen(true)}>
+        Submit
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+          {Object.entries(formValues).map((element: any)=>{
+            return(
+              <>
+              <DialogContent>
+                <DialogContentText>{<b>{element[0]}</b>}:{element[1]}</DialogContentText>
+              </DialogContent>
+              </>
+          )})}
+      </Dialog>
+    </div>
+  );
+}
